7891e7acef813167452354732595b80c1112c678
priyansh-gupta23 <123014888+priyansh-gupta23@users.noreply.github.com>
2023-10-27 15:48:21 +0530

    axios commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from "react";
+import axios from '@/utils/axios'
 
 export default function index() {
   const [message, setMessage] = useState("Loading...");
 
-  useEffect(() => {
-    fetch("https://form-server-6gpy.onrender.com/")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message));
-  }, []);
+  const submitHandler = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    try {
+      const {data} = await axios.get("/");
+      setMessage(data.message)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
 
   return (
     <>
+      <form onSubmit={submitHandler}>
+        <button className="btn btn-sucess" type="submit">submit</button>
+      </form>
       <h1>{message}</h1>
     </>
   );
